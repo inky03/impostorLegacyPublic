2fc40d2ac3603428b9e84cde43eee801d3fc3ea1
ashley <84390581+inky03@users.noreply.github.com>
2026-08-25 12:16:40 -0600

    Update bf-ghost.hx
    redundant

--- assets/legacy/data/characters/bf-ghost.hx
+++ assets/legacy/data/characters/bf-ghost.hx
@@ -3,13 +3,3 @@ function onLoad()
 	parent.useRenderTexture = true;
 	parent.alpha = .8;
 }
-
-function onUpdate(elapsed:Float):Void
-{
-	if (FlxG.keys.justPressed.SPACE && boyfriend.curCharacter == 'bf-ghost' && boyfriend.getAnimName() == 'idle-loop')
-	{
-		boyfriend.playAnim('hey');
-		boyfriend.specialAnim = true;
-		boyfriend.holding = true;
-	}
-}
